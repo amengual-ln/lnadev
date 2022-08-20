dc381e133a68d9c4c93716332e759566866a4cf3
Lautaro Amengual <amengual.ln@gmail.com>
2022-08-20 11:35:34 -0300

    Apply max width to layout and hide header on mobile

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,4 @@
-<header class="fixed w-full flex justify-center p-8 backdrop-blur-sm z-50">
+<header class="hidden fixed w-full justify-center p-8 backdrop-blur-sm z-50 sm:flex">
 	<nav>
 		<ul class="flex gap-8">
 			<li><a href="/">Home</a></li>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+	import Header from "../components/Header.svelte"
 </script>
 
-<slot />
+<Header />
+<div class="w-full max-w-screen-lg m-auto p-4">
+	<slot />
+</div>
+
