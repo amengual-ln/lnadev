f2d0f67b8b3a845dff95e0d8d1233d04c6bc3663
Lautaro Amengual <amengual.ln@gmail.com>
2022-08-25 01:35:06 -0300

    Add floating tech svgs in hero Add component with tech logos and custom animation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.css';
-	import Header from "../components/Header.svelte"
+	import Header from '../components/Header.svelte';
+	import Technologies from '../components/Technologies.svelte';
 </script>
 
 <Header />
 <div class="w-full max-w-screen-lg m-auto p-4">
 	<slot />
+	<Technologies />
 </div>
-
