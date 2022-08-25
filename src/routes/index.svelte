1e46ec55a00977f0d516fd3a66231d53d7ed92cc
Lautaro Amengual <amengual.ln@gmail.com>
2022-08-25 01:33:07 -0300

    lint and fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Header from "../components/Header.svelte";
 	import Hero from "../components/Hero.svelte";
 	import Experience from "../components/Experience.svelte";
 	import Projects from "../components/Projects.svelte";
