f65ed69532fa5c639dfda0a3ac4a1eff3a6cc730
Lautaro Amengual <amengual.ln@gmail.com>
2023-01-26 15:38:49 -0300

    Add email to the contact icons in hero :)

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -10,9 +10,14 @@
 			<b>Coderhouse</b>
 		</a>
 	</span>
-	<a href="https://www.linkedin.com/in/lautaroamengual" target="_blank">
-		<img src="/linkedin.svg" alt="Linked In" class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80" />
-	</a>
+	<div class="flex gap-3">
+		<a href="https://www.linkedin.com/in/lautaroamengual" target="_blank">
+			<img src="/linkedin.svg" alt="Linked In" class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80" />
+		</a>
+		<a href="mailto:amengual.ln@gmail.com" target="_blank">
+			<img src="/mail.svg" alt="Linked In" class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80" />
+		</a>
+	</div>
 	<a href="#experience" class="absolute p-4 m-2 bottom-0 text-center animate-bounce dark:invert dark:opacity-80">
 		<img src="/chevron-double-down.svg" alt="Ver más" />
 	</a>
