d93ce2fce7d664f137fb656e3cf07826324e0420
Lautaro Amengual <amengual.ln@gmail.com>
2023-01-26 15:38:13 -0300

    Hide projects section until it's done

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,7 +9,6 @@
 		<ul class="flex gap-8">
 			<li><a href="/">Home</a></li>
 			<li><a href="#experience">Experience</a></li>
-			<li><a href="#projects">Projects</a></li>
 		</ul>
 	</nav>
 </header>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Hero from '../components/Hero.svelte';
 	import Experience from '../components/Experience.svelte';
-	import Projects from '../components/Projects.svelte';
 
 	import { onMount } from 'svelte';
 
@@ -20,5 +19,3 @@
 <Hero />
 
 <Experience />
-
-<Projects />
