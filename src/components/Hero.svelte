81690b760874f50154d42916c869c3151f26028f
Lautaro Amengual <amengual.ln@gmail.com>
2023-02-14 20:31:21 -0300

    Add new job in Hero :)

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,24 +1,40 @@
-<section
-	id="home"
-	class="relative flex flex-col gap-4 justify-center items-center min-h-screen text-center z-40"
->
-	<h1 class="text-5xl font-medium">Hello! I'm Lautaro Amengual</h1>
-	<h2 class="text-3xl">Frontend Engineer</h2>
-	<span>
-		Co-founder @ <b>UPYA</b> | JS Mentor @
-		<a href="https://www.coderhouse.com" target="_blank">
-			<b>Coderhouse</b>
-		</a>
-	</span>
-	<div class="flex gap-3">
-		<a href="https://www.linkedin.com/in/lautaroamengual" target="_blank">
-			<img src="/linkedin.svg" alt="Linked In" class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80" />
-		</a>
-		<a href="mailto:amengual.ln@gmail.com" target="_blank">
-			<img src="/mail.svg" alt="Linked In" class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80" />
+<section id="home" class="flex flex-col justify-center min-h-screen text-center z-40">
+	<article class="flex flex-col gap-4 justify-center items-center">
+		<h1 class="text-5xl font-medium">Hello! I'm Lautaro Amengual</h1>
+		<h2 class="text-3xl">Frontend Engineer</h2>
+		<span>
+			Frontend Engineer @
+			<a href="https://www.talenthackers.net" target="_blank">
+				<b>TalentHackers</b>
+			</a>
+			| JS Mentor @
+			<a href="https://www.coderhouse.com" target="_blank">
+				<b>Coderhouse</b>
+			</a>
+		</span>
+		<div class="flex gap-3">
+			<a href="https://www.linkedin.com/in/lautaroamengual" target="_blank">
+				<img
+					src="/linkedin.svg"
+					alt="Linked In"
+					class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80"
+				/>
+			</a>
+			<a href="mailto:amengual.ln@gmail.com" target="_blank">
+				<img
+					src="/mail.svg"
+					alt="Linked In"
+					class="rounded-md transition ring-gray-400 hover:ring dark:invert dark:opacity-80"
+				/>
+			</a>
+		</div>
+	</article>
+	<div class="flex justify-center">
+		<a
+			href="#experience"
+			class="absolute p-4 m-2 bottom-0 animate-bounce dark:invert dark:opacity-80"
+		>
+			<img src="/chevron-double-down.svg" alt="Ver más" />
 		</a>
 	</div>
-	<a href="#experience" class="absolute p-4 m-2 bottom-0 text-center animate-bounce dark:invert dark:opacity-80">
-		<img src="/chevron-double-down.svg" alt="Ver más" />
-	</a>
 </section>
