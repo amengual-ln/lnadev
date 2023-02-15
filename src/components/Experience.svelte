c61dd41b44bc08799f5bdad4efee57d135c86c8d
Lautaro Amengual <amengual.ln@gmail.com>
2023-02-15 08:22:36 -0300

    Replace the dot with an svg (timeline) The ascii dot I was using looks a lot different in mac and mobile :(

--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -11,17 +11,22 @@
 	<h2 class="text-3xl mb-8">Experience</h2>
 
 	<div class="w-full">
+		<!-- TODO: move timeline to its own component -->
 		{#each JOBS as job, i}
 			{#if i % 2 === 0}
 				<article class="flex gap-8 items-center">
 					<div class="flex-1"><JobCard {job} /></div>
-					<div class="flex-0 opacity-50 {i === JOBS.length - 1 ? '' : 'timeline'}">⏺</div>
+					<div class="flex-0 opacity-50 {i === JOBS.length - 1 ? '' : 'timeline'}">
+						<img src="/dot.svg" alt="⏺" class="timeline-img" />
+					</div>
 					<div class="flex-1" />
 				</article>
 			{:else}
 				<article class="flex gap-8 items-center">
 					<div class="flex-1" />
-					<div class="flex-0 opacity-50 {i === JOBS.length - 1 ? '' : 'timeline'}">⏺</div>
+					<div class="flex-0 opacity-50 {i === JOBS.length - 1 ? '' : 'timeline'}">
+						<img src="/dot.svg" alt="⏺" class="timeline-img" />
+					</div>
 					<div class="flex-1"><JobCard {job} /></div>
 				</article>
 			{/if}
@@ -34,8 +39,11 @@
 		content: '';
 		border-left: 1px solid gray;
 		position: absolute;
-		transform: translate(-80%, 22%);
+		transform: translate(-80%, 18%);
 		left: 50%;
 		height: 10em;
+	}
+	.timeline-img {
+		height: 11px;
 	}
 </style>
