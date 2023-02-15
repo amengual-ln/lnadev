7667ee5f2aaf79e27f488240914c7b35d290704e
Lautaro Amengual <amengual.ln@gmail.com>
2023-02-14 21:08:29 -0300

    Add jobs array and company logos

---
+++ src/jobs.js
@@ -0,0 +1,58 @@
+export const JOBS = [
+    {
+        company: 'TalentHackers',
+        color: 'blue-talenthackers',
+        url: 'https://talenthackers.net/',
+        imgUrl: '/talenthackers.webp',
+        position: 'Front-end Engineer',
+        from: 'Feb 2023',
+        to: 'Present',
+        skills: ['vue', 'js'],
+    },
+    {
+        company: 'Coderhouse',
+        color: 'black',
+        url: 'https://coderhouse.com/',
+        imgUrl: '/coderhouse.svg',
+        position: 'JS Mentor',
+        from: 'Jun 2022',
+        to: 'Present',
+        skills: ['js'],
+    },
+    {
+        company: '9net',
+        color: 'red-9net',
+        url: 'https://www.9net.com.ar',
+        imgUrl: '/9net.png',
+        position: 'Fullstack Engineer',
+        from: 'Oct 2018',
+        to: 'Oct 2020',
+        skills: ['vue', 'js', 'php'],
+    }
+]
+
+{/* <div class="relative w-full group">
+		<div class="absolute inset-0 bg-indigo-upya rounded-lg blur opacity-75" />
+		<article
+			class="relative w-full bg-indigo-upya text-white p-8 rounded-lg shadow-xl shadow-purple-200 dark:shadow-none"
+		>
+			<h3 class="text-3xl mb-2">
+				<a href="https://www.coderhouse.com" target="_blank" class="font-medium"> UPYA </a>
+			</h3>
+			<span>
+				<b>Co-Founder, Frontend Engineer</b>
+				| Oct 2020 - Present
+			</span>
+			<p class="my-4">Designing and developing a platform for education management</p>
+
+			<div class="flex justify-between mt-6">
+				<div class="flex gap-2 h-12">
+					<Icon type="react" fill="#fff" size={6} />
+					<Icon type="next" fill="#fff" size={6} />
+					<Icon type="nest" fill="#fff" size={6} />
+					<Icon type="ts" fill="#fff" size={6} />
+				</div>
+				<!-- <Button>Read more</Button> -->
+			</div>
+		</article>
+	</div> */}
