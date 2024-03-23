b1c0b1b374b1e9070a3f8d33c2ea14cb618b0957
Lautaro Amengual <amengual.ln@gmail.com>
2024-03-23 18:17:38 -0300

    Grayish' background in light mode. Smaller text in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer
-    class="w-full justify-center items-center p-8 sm:flex text-gray-800/50 dark:bg-neutral-900 dark:text-gray-200/50"
+    class="w-full justify-center items-center p-8 text-sm flex text-gray-800/50 dark:bg-neutral-900 dark:text-gray-200/50"
 >
     <pre>Powered by Svelte and my {`<3`} for web dev</pre>
 </footer>
